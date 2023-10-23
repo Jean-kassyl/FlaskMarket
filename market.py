cca32d1ce7b82fe03ce351a99b9641645a50314e
Jean-kassyl <kassylmac@gmail.com>
2023-10-23 18:04:20 +0100

    setting bootstrap5 and styling the navbar

--- market.py
+++ market.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def Hello_world():
-    return "Hello world"
+    return render_template("home.html", title= "homePage")
+
+@app.route('/market')
+def market_page():
+    return render_template('market.html', title="marketPage")
