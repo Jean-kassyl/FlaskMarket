f6f769c6bbf76d1518eb85e4da48ef30a4a8c5bb
Jean-kassyl <kassylmac@gmail.com>
2023-10-27 12:21:49 +0100

    setting the user table and define relationship

--- market/models.py
+++ market/models.py
@@ -1,5 +1,14 @@
 from market import db
 
+class Users(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    user_email = db.Column(db.String(lenght=60), nulllable=False, unique=True)
+    hash_pass = db.Column(db.String(), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('Item', backref="owner_user", lazy=True)
+
+
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -7,6 +16,7 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(), nullable=False)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return "Item N°{}, name: {} )".format(self.id, self.name)
